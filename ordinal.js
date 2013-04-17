ef63c6a42a8842382d8cb8b66de0197bda844a91
John Pedrie <johnpedrie@quickenloans.com>
2013-04-17 13:09:35 -0400

    Added component.json and made the filter into its own module.

--- ordinal.js
+++ ordinal.js
@@ -7,7 +7,7 @@
 // h/t http://ecommerce.shopify.com/c/ecommerce-design/t/ordinal-number-in-javascript-1st-2nd-3rd-4th-29259
 
 
-angular.module('filters', []).filter('ordinal', function() {
+angular.module('ordinal', []).filter('ordinal', function() {
   return function(input) {
     var s=["th","st","nd","rd"],
     v=input%100;
