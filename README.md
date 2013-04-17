7635b3897a214520af18053a65e0e40f6be3cc24
John Pedrie <johnpedrie@quickenloans.com>
2013-04-17 12:58:31 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# AngularJS Ordinal Number Filter
+
+
+    {{numericalValue | ordinal}}
+
+Converts e.g 1 -> 1st, 2 -> 2nd, 3 -> 3rd, 4 -> 4th.
+
+## Hat tip
+The snippet to create an ordinal number was found online [here](http://ecommerce.shopify.com/c/ecommerce-design/t/ordinal-number-in-javascript-1st-2nd-3rd-4th-29259)
---
+++ ordinal.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Ordinal Number Filter
+// ---------------------
+// This filter takes a number and returns its ordinal value
+// i.e. 1 -> 1st, 2 -> 2nd, etc.
+// h/t http://ecommerce.shopify.com/c/ecommerce-design/t/ordinal-number-in-javascript-1st-2nd-3rd-4th-29259
+
+
+angular.module('filters', []).filter('ordinal', function() {
+  return function(input) {
+    var s=["th","st","nd","rd"],
+    v=input%100;
+    return input+(s[(v-20)%10]||s[v]||s[0]);
+  }
+});
