569b7ff778b2e56c3c2f8e591e38a53787f78a77
John Dennis Pedrie <jdpedrie@users.noreply.github.com>
2014-07-08 09:32:03 -0400

    Handle non-numeric values
    If given `input` is not a number, return `input` with no changes. `'100'` and `100` both pass the test.

--- ordinal.js
+++ ordinal.js
@@ -8,9 +8,14 @@
 
 
 angular.module('ordinal', []).filter('ordinal', function() {
-  return function(input) {
+  var ordinal = function(input) {
+    // Only process numeric values.
+    if (isNaN(input) || input === null) return input;
+
     var s=["th","st","nd","rd"],
     v=input%100;
     return input+(s[(v-20)%10]||s[v]||s[0]);
   }
+
+  return ordinal;
 });
