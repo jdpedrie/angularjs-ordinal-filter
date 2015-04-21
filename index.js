7b7faa2df907d9df4cd0e626744c97139ebf23c4
John Pedrie <johnpedrie@quickenloans.com>
2015-04-21 07:17:01 -0400

    Fix #5 and #6

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var ordinal = require('ordinal');
-
 angular
   .module('ordinal', [])
 
@@ -9,6 +7,12 @@ angular
     // Take a number and returns its ordinal value
     // i.e. 1 -> 1st, 2 -> 2nd, etc.
     return function(input) {
-      return angular.isNumber(input) ? ordinal.english(input) : input;
+      // Only process numeric values.
+      if (isNaN(input) || input === null) return input;
+
+      var s=["th","st","nd","rd"];
+      var v=input%100;
+
+      return input+(s[(v-20)%10]||s[v]||s[0]);
     };
   });
--- test.js
+++ test.js
@@ -13,11 +13,37 @@ describe('ordinal', function() {
     });
   });
 
-  it('returns original non-numeric input', function() {
-    assert.equal(ordinalFilter('Not a number'), 'Not a number');
+  var tests = [
+    ['1', '1st'],
+    ['2', '2nd'],
+    ['3', '3rd'],
+    ['4', '4th'],
+    ['5', '5th'],
+    ['6', '6th'],
+    ['7', '7th'],
+    ['8', '8th'],
+    ['9', '9th'],
+    ['10', '10th'],
+    ['11', '11th'],
+    ['12', '12th'],
+    ['13', '13th'],
+    ['14', '14th'],
+    ['15', '15th'],
+    ['16', '16th'],
+    ['17', '17th'],
+    ['18', '18th'],
+    ['19', '19th'],
+    ['20', '20th']
+  ];
+
+  tests.forEach(function(test) {
+    console.log
+    it ('should take ' + test[0] + ' and return ' + test[1], function() {
+      assert.equal(ordinalFilter(test[0]), test[1]);
+    })
   });
 
-  it('returns ordinal string from numeric input', function() {
-    assert.equal(ordinalFilter(1), '1st');
+  it('returns original non-numeric input', function() {
+    assert.equal(ordinalFilter('Not a number'), 'Not a number');
   });
 });
